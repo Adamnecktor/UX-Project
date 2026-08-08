68168416989daca622e6013e40cae71361f99c56
Anime Enjoyer <251304T@mymail.nyp.edu.sg>
2026-08-08 20:54:36 +0800

    V4.3
    ALOT OF CHANGES TOO LAZY TO SAY

--- Personal_Website/Javascript/External.js
+++ Personal_Website/Javascript/External.js
@@ -1,4 +1,35 @@
-// --- Side Nav Toggle Script ---
+//--------------Form_validation---------------//
+
+    function form_validation() {
+
+        const form = document.getElementById("form");
+        const name = document.getElementById("N").value;
+        const email = document.getElementById("E").value;
+
+
+        let valid = true;
+        let namecheck = !isNaN(name);
+
+        if (namecheck)  {
+            document.getElementById("err_N").innerHTML = "<p>Please enter valid name</p>";
+            valid = false;
+
+        }
+
+        if (!email.includes("@") || !email.includes(".")) {
+            document.getElementById("err_E").innerHTML = "<p>Please enter valid email</p>";
+            valid = false;
+        }
+
+        if (!valid) {
+            event.preventDefault();
+            return false
+        }
+        
+    return true;
+    }
+
+// --- Side Nav Toggle Script ---//
         const toggleBtn = document.getElementById('navToggleBtn');
         const sideMenu = document.getElementById('sideMenu');
 
